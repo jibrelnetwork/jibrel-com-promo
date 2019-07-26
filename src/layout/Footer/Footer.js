0fa23690acdbd6d9828cd0b1fc18bea9a9c0b777
Eugene <dragonesis@ya.ru>
2019-07-26 17:51:11 +0300

    Created footer

---
+++ src/layout/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { Container } from '/components'
+
+import style from './Footer.scss'
+import { 
+  logoFooter,
+  twitter,
+  medium,
+  linkedin,
+  instagram 
+} from '/assets/icons/'
+
+function Footer() {
+  return (
+    <Container className={style.footer} tag='footer'>
+      <div className={style.social}>
+        <a href='#' className={style.link} target='_blank'><img src={twitter} alt='twitter'/></a>
+        <a href='#' className={style.link} target='_blank'><img src={medium} alt='medium'/></a>
+        <a href='#' className={style.link} target='_blank'><img src={linkedin} alt='linkedin'/></a>
+        <a href='#' className={style.link} target='_blank'><img src={instagram} alt='instagram'/></a>
+      </div>
+      <div>
+        <a className={style.logo}><img src={logoFooter} /></a>
+        <div className={style.info}>Copyright © 2019 Jibrel Network. All rights reserved.</div>
+      </div>
+    </Container>
+  )
+}
+
+export default React.memo(Footer)
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from './Header/Header.js'
+import Footer from './Footer/Footer.js'
 
 export default function Layout ({ children }) {
   return (
-    <div>
+    <>
       <Header />
-      <div>
+      <main>
         {children}
-      </div>
-    </div>
+      </main>
+      <Footer />
+    </>
   )
 }
