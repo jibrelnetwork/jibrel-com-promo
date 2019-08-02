3f170718e30f101d75e3f97f0b93d9b0fcfb0eac
Evgeny Vanchugov <dragonesis@ya.ru>
2019-08-02 16:46:12 +0300

    Actualization social links

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,12 +3,6 @@ export { default as arrowDown } from '/assets/icons/ic_arrow_down_24.svg'
 export { default as arrowRightWhite } from '/assets/icons/ic_arrow_right_white.svg'
 export { default as arrowRightBlue } from '/assets/icons/ic_arrow_right_blue.svg'
 
-// Social
-export { default as instagram } from '/assets/icons/ic_instagram_24.svg'
-export { default as linkedin } from '/assets/icons/ic_linkedin_24.svg'
-export { default as medium } from '/assets/icons/ic_medium_24.svg'
-export { default as twitter } from '/assets/icons/ic_twitter_24.svg'
-
 // Logo
 export { default as logoHeader } from '/assets/icons/ic_logo_colored_32.svg'
 export { default as logoFooter } from '/assets/icons/ic_logo_mono_32.svg'
