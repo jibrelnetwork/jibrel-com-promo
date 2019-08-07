73edbbe49960b3902bdd6a038d5c5674d26b161b
Evgeny Vanchugov <dragonesis@ya.ru>
2019-08-07 14:31:38 +0300

    Fix mistakes

--- src/components/Animation/index.js
+++ src/components/Animation/index.js
@@ -8,10 +8,6 @@ class Animation extends React.PureComponent {
   state = {
     animationData: null,
   }
-  
-  constructor(props) {
-    super(props)
-  }
 
   switchAnimation = (value) => {
     switch (value) {
@@ -28,29 +24,24 @@ class Animation extends React.PureComponent {
     }
   }
 
-  animationPlanet = () => Promise
-    .all([import('./animations/planet.json')])
-    .then(([animationData]) => { 
+  animationPlanet = () => import('./animations/planet.json')
+    .then((animationData) => { 
       this.setState({ animationData })
     })
-  animationTranparency = () => Promise
-    .all([import('./animations/tranparency.json')])
-    .then(([animationData]) => { 
+  animationTranparency = () => import('./animations/tranparency.json')
+    .then((animationData) => { 
       this.setState({ animationData })
     })
-  animationRadar = () => Promise
-    .all([import('./animations/radar.json')])
-    .then(([animationData]) => {
+  animationRadar = () => import('./animations/radar.json')
+    .then((animationData) => {
       this.setState({ animationData }) 
     })
-  animationHourglass = () => Promise
-    .all([import('./animations/hourglass.json')])
-    .then(([animationData]) => {
+  animationHourglass = () => import('./animations/hourglass.json')
+    .then((animationData) => {
       this.setState({ animationData }) 
     })
-  animationError = () => Promise
-    .all([import('./animations/error.json')])
-    .then(([animationData]) => {
+  animationError = () => import('./animations/error.json')
+    .then((animationData) => {
       this.setState({ animationData }) 
     })
   
