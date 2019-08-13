c631ca8af0950e1e7ae2a3ef085909ef1ba3ad8c
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-08-13 18:15:27 +0300

    Escape external variable in messages
    Add escape handler to i18n helper

--- emails/src/helpers/i18n.js
+++ emails/src/helpers/i18n.js
@@ -1,5 +1,8 @@
 const _ = require('lodash')
 
+const ICU_ESCAPE_RE = /'{(.+?)}'/g
+
 module.exports = function (translation, id) {
-  return _.get(translation, [id], '⚠️')
+  const message = _.get(translation, [id]) || '⚠️'
+  return message.replace(ICU_ESCAPE_RE, (match, p) => `{${p}}`)
 }
