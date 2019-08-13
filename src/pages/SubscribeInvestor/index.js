d2e12748e9cc00c2f7a0a82f70da691ab380f548
Evgeny Vanchugov <dragonesis@ya.ru>
2019-08-13 16:23:52 +0300

    Sending analytic and forms without final-form

--- src/pages/SubscribeInvestor/index.js
+++ src/pages/SubscribeInvestor/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import cc from 'classcat'
-import { Form, Field } from 'react-final-form'
 import { useI18n } from '/hooks/i18n'
 import { I18nContext } from '/app/i18n-provider'
 import { sendEventGTM, sendEventGTMAccountType } from '/utils/send-gtm'
@@ -21,6 +20,7 @@ import link from '/theme/link.css'
 export default function SubscribeFounder() {
   const i18n = useI18n()
   const [resultOfSending, setResultOfSending] = useState(null)
+  const [submitting, setSubmitting] = useState(false)
   const [fieldsValue, setFieldsValue] = useState({
     name: '',
     email: '',
@@ -57,17 +57,21 @@ export default function SubscribeFounder() {
     sendEventGTM('lead', 'completeForm', 'chooseCountry')
   }
 
-  function handleFormSubmit(values) {
+  function handleFormSubmit(e) {
+    console.log(e)
+    
+    e.preventDefault()
     const data = {
-      'email': values.email,
-      'name': values.name,
+      'email': fieldsValue.email,
+      'name': fieldsValue.name,
       'fields': {
-        'country': fieldsValue.user_type === 'investor_individual' ? values.country : undefined,
-        'user_type': values.user_type,
-        'company': values.company,
-        'language': values.language,
+        'country': fieldsValue.user_type === 'investor_individual' ? fieldsValue.country : undefined,
+        'user_type': fieldsValue.user_type,
+        'company': fieldsValue.company,
+        'language': fieldsValue.language,
       }
     }
+    setSubmitting(true)
     fetch('/api/subscribe', {
       method: 'POST',
       body: JSON.stringify(data),
@@ -75,10 +79,12 @@ export default function SubscribeFounder() {
       .then(() => {
         setResultOfSending('success')
         sendEventGTM('lead', 'sendSuccess', 'mainForm')
+        setSubmitting(false)
       })
       .catch(() => {
         setResultOfSending('error')
         sendEventGTM('lead', 'sendDenied', 'mainForm')
+        setSubmitting(false)
       })
   }  
 
@@ -91,135 +97,114 @@ export default function SubscribeFounder() {
             <LanguageLink routeName='SubscribeFounder' className={link.link}>
               {i18n._('SubscribeInvestor.head.goToSignUp')}
             </LanguageLink>
-            <Form
-              render={({
-                handleSubmit,
-                submitting,
-              }) => (
-                <form className={form.form} onSubmit={handleSubmit}>
-                  <div className={form.box}>
-                    <h2 className={form.title}>{i18n._('SubscribeInvestor.input.typeOfAccount.title')}</h2>
-                    <label className={form.radio}>
-                      <Field
-                        name='user_type'
-                        component='input'
-                        type='radio'
-                        value='investor_individual'
-                        required
-                        disabled={submitting}
-                        checked={fieldsValue.user_type === 'investor_individual'}
-                        onChange={ onChangeTypeOfAccount }
-                      />
-                      <div className={form.radioBox}>
-                        <p className={form.radioTitle}>{i18n._('SubscribeInvestor.input.typeOfAccount.option.individual.title')}</p>
-                        <p className={form.radioDescription}>{i18n._('SubscribeInvestor.input.typeOfAccount.option.individual.info')}</p>
-                      </div>
-                    </label>            
-                    <label className={form.radio}>
-                      <Field
-                        name='user_type'
-                        component='input'
-                        type='radio'
-                        value='investor_organization'
-                        required
-                        disabled={submitting}
-                        checked={fieldsValue.user_type === 'investor_organization'}
-                        onChange={ onChangeTypeOfAccount }
-                      />
-                      <div className={form.radioBox}>
-                        <p className={form.radioTitle}>{i18n._('SubscribeInvestor.input.typeOfAccount.option.organization.title')}</p>
-                        <p className={form.radioDescription}>{i18n._('SubscribeInvestor.input.typeOfAccount.option.organization.info')}</p>
-                      </div>
-                    </label>
+            <form className={form.form} onSubmit={handleFormSubmit}>
+              <div className={form.box}>
+                <h2 className={form.title}>{i18n._('SubscribeInvestor.input.typeOfAccount.title')}</h2>
+                <label className={form.radio}>
+                  <input
+                    name='user_type'
+                    type='radio'
+                    value='investor_individual'
+                    required
+                    disabled={submitting}
+                    checked={fieldsValue.user_type === 'investor_individual'}
+                    onChange={ onChangeTypeOfAccount }
+                  />
+                  <div className={form.radioBox}>
+                    <p className={form.radioTitle}>{i18n._('SubscribeInvestor.input.typeOfAccount.option.individual.title')}</p>
+                    <p className={form.radioDescription}>{i18n._('SubscribeInvestor.input.typeOfAccount.option.individual.info')}</p>
                   </div>
-                  {fieldsValue.user_type === 'investor_individual' ? (
-                      <>
-                        <label className={form.box}>
-                          <h2 className={form.title}>{i18n._('SubscribeInvestor.input.fullName.title')}</h2>
-                          <Field
-                            name='name'
-                            component='input'
-                            required
-                            className={form.input}
-                            disabled={submitting}
-                            initialValue={fieldsValue.name}
-                            onChange={onChageInput}
-                          />
-                        </label>
-                        <label className={form.box}>
-                          <h2 className={form.title}>{i18n._('SubscribeInvestor.input.countryOfResidence.title')}</h2>
-                          <Field
-                            name='country'
-                            component='select'
-                            required
-                            disabled={submitting}
-                            className={form.input}
-                            initialValue={fieldsValue.country}
-                            onChange={onChangeCountry}
-                          >
-                            <option>{i18n._('SubscribeInvestor.input.countryOfResidence.empty')}</option>
-                            {COUNTRIES.map(item => <option value={item.id} key={item.id}>{item.title}</option> )}
-                          </Field>
-                        </label>
-                      </>
-                  ) : (
-                      <>
-                      <label className={form.box}>
-                        <h2 className={form.title}>{i18n._('SubscribeInvestor.input.fullNameOrganization.title')}</h2>
-                        <Field
-                          name='company'
-                          component='input'
-                          required
-                          disabled={submitting}
-                          className={form.input}
-                          initialValue={fieldsValue.company}
-                          onChange={onChageInput}
-                        />
-                      </label>
-                      <label className={form.box}>
-                        <h2 className={form.title}>{i18n._('SubscribeInvestor.input.contactNameOrganization.title')}</h2>
-                        <Field
-                          name='name'
-                          component='input'
-                          required
-                          disabled={submitting}
-                          className={form.input}
-                          initialValue={fieldsValue.name}
-                          onChange={onChageInput}
-                        />
-                      </label>
-                      </>
-                  )}
-                    
+                </label>            
+                <label className={form.radio}>
+                  <input
+                    name='user_type'
+                    type='radio'
+                    value='investor_organization'
+                    required
+                    disabled={submitting}
+                    checked={fieldsValue.user_type === 'investor_organization'}
+                    onChange={ onChangeTypeOfAccount }
+                  />
+                  <div className={form.radioBox}>
+                    <p className={form.radioTitle}>{i18n._('SubscribeInvestor.input.typeOfAccount.option.organization.title')}</p>
+                    <p className={form.radioDescription}>{i18n._('SubscribeInvestor.input.typeOfAccount.option.organization.info')}</p>
+                  </div>
+                </label>
+              </div>
+              {fieldsValue.user_type === 'investor_individual' ? (
+                <>
                   <label className={form.box}>
-                    <h2 className={form.title}>{i18n._('SubscribeInvestor.input.email.title')}</h2>
-                    <Field
-                      name='email'
-                      component='input'
+                    <h2 className={form.title}>{i18n._('SubscribeInvestor.input.fullName.title')}</h2>
+                    <input
+                      name='name'
                       required
                       className={form.input}
-                      initialValue={fieldsValue.email}
+                      disabled={submitting}
                       onChange={onChageInput}
                     />
                   </label>
-                  <I18nContext.Consumer>
-                    {({ languageCode }) => (   
-                      <Field
-                        name='language'
-                        component='input'
-                        type='hidden'
-                        required
-                        initialValue={languageCode}
-                        className={form.input}
-                        disabled
-                      />
-                    )}
-                  </I18nContext.Consumer>
-                  <button className={cc([button.button, button.blue, button.normal, form.submit])} disabled={submitting}>{i18n._('SubscribeInvestor.form.action.continue')}</button>
-                </form>
+                  <label className={form.box}>
+                    <h2 className={form.title}>{i18n._('SubscribeInvestor.input.countryOfResidence.title')}</h2>
+                    <select
+                      name='country'
+                      required
+                      disabled={submitting}
+                      className={form.input}
+                      onChange={onChangeCountry}
+                    >
+                      <option value=''>{i18n._('SubscribeInvestor.input.countryOfResidence.empty')}</option>
+                      {COUNTRIES.map(item => <option value={item.id} key={item.id}>{item.title}</option> )}
+                    </select>
+                  </label>
+                </>
+              ) : (
+                <>
+                <label className={form.box}>
+                  <h2 className={form.title}>{i18n._('SubscribeInvestor.input.fullNameOrganization.title')}</h2>
+                  <input
+                    name='company'
+                    required
+                    disabled={submitting}
+                    className={form.input}
+                    onChange={onChageInput}
+                  />
+                </label>
+                <label className={form.box}>
+                  <h2 className={form.title}>{i18n._('SubscribeInvestor.input.contactNameOrganization.title')}</h2>
+                  <input
+                    name='name'
+                    required
+                    disabled={submitting}
+                    className={form.input}
+                    onChange={onChageInput}
+                  />
+                </label>
+                </>
               )}
-              onSubmit={handleFormSubmit}
-            />
+                    
+              <label className={form.box}>
+                <h2 className={form.title}>{i18n._('SubscribeInvestor.input.email.title')}</h2>
+                <input
+                  name='email'
+                  required
+                  className={form.input}
+                  onChange={onChageInput}
+                />
+              </label>
+              <I18nContext.Consumer>
+                {({ languageCode }) => (   
+                  <input
+                    name='language'
+                    type='hidden'
+                    required
+                    value={languageCode}
+                    className={form.input}
+                    disabled
+                  />
+                )}
+              </I18nContext.Consumer>
+              <button className={cc([button.button, button.blue, button.normal, form.submit])} disabled={submitting}>{i18n._('SubscribeInvestor.form.action.continue')}</button>
+            </form>
           </div>
         )}
         {resultOfSending === 'success' && (
--- src/utils/send-gtm.js
+++ src/utils/send-gtm.js
@@ -1,6 +1,5 @@
 
 function sendEventGTM(eventCategory, eventAction, eventLabel) {
-  console.log(eventCategory, eventAction, eventLabel)
   window.dataLayer.push({
     event: 'autoEvent',
     eventCategory: eventCategory,
@@ -10,19 +9,17 @@ function sendEventGTM(eventCategory, eventAction, eventLabel) {
 }
 
 function sendEventGTMAccountType(eventCategory, eventAction, eventLabel, accountType) {
-  console.log(eventCategory, eventAction, eventLabel, accountType.replace('investor_', ''))
   window.dataLayer.push({
     event: 'autoEvent',
     eventCategory: eventCategory,
     eventAction: eventAction,
     eventLabel: eventLabel,
-    accountType: accountType.replace('investor_', '')
+    accountType: accountType
   })
 }
 
 function sendVirtualPageviewGTM(virtualTitle, virtualUrl) {
   const virtualHost = window.location.host
-  console.log(virtualHost, virtualTitle, virtualUrl)
   window.dataLayer.push({
     event: 'virtualPageview',
     virtualHost: virtualHost,
