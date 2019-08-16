b40481cca216db82c3662183c95e96ca61294e1a
Evgeny Vanchugov <dragonesis@ya.ru>
2019-08-16 13:47:03 +0300

    JCWEB-38 BUG — No flag emojis in Edge 18

--- src/components/form/CountrySelect/index.js
+++ src/components/form/CountrySelect/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cc from 'classcat'
 import * as PropTypes from 'prop-types'
 
@@ -16,10 +16,15 @@ export default function CountrySelect ({
   isDisabled,
   isRequired,
 }) {
-  function setCountry(value) {
+  // const [currentCountry, setCurrentCountry] = useState(null)
+
+  function getFlag(value) {
     const currentCountry = COUNTRIES.filter(country => country.id === value)
-    
-    return `${currentCountry[0].flag} ${currentCountry[0].title}`
+    return currentCountry[0].flag
+  }
+  function getCountry(value) {
+    const currentCountry = COUNTRIES.filter(country => country.id === value)
+    return currentCountry[0].title
   }
   return (
     <div className={cc([style.field, className])}>
@@ -35,7 +40,14 @@ export default function CountrySelect ({
         {COUNTRIES.map(item => <option value={item.id} key={item.id}>{item.title}</option> )}
       </select>     
       <div className={style.toggle}>
-        {value ? setCountry(value) : <span className={style.placeholder}>{placeholder}</span>}
+        {value ? (
+          <>
+          <span className={style.flag}>{getFlag(value)}</span> 
+          <span className={style.name}>{getCountry(value)}</span>
+          </>
+        ) : (
+          <span className={style.placeholder}>{placeholder}</span>
+        )}
         <svg className={style.arrow} width='24' height='24' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' clipRule='evenodd' d='M12.707 14.707a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 1.414-1.414L12 12.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4z' /></svg>
       </div>               
       <p className={style.label}>{label}</p>
